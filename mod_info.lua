1e915476cef8f9052fa5ee258d206db79c3da70a
vjt4 <54646079+vjt4@users.noreply.github.com>
2026-07-29 09:14:31 -0500

    fixed mod_info

--- mod_info.lua
+++ mod_info.lua
@@ -1,22 +1,15 @@
 name = "ImprovedSelection"
 version = 1
 copyright = ""
-[[
+description = [[
 REQUIRES REUI
-Four hotkeys for air combat management.
-"Select Fighters" selects all friendly fighters, excluding any that are currently excluded.
-"Select Bombers" selects all friendly bombers, excluding any that are currently excluded.
-"Select Gunships" selects all friendly gunships, excluding any that are currently excluded.
+4 Hotkeys for air combat management.
+Select All Fighters or Gunships or Bombers
 "Exclude / Include Air Combat from Selection" toggles whether the selected fighters, gunships, and bombers are excluded from the selection hotkeys.
 If any selected unit is already excluded, all selected units are included again.
 Hotkeys for on screen land management
-Select all onscreen mml/sniper
-Select all onscreen non-exp Direct Fire
-Select all on screen AA
-Select all on screen mobile shields/decievers
-Select all on screen direct fire exp
-
-Hotkey to add selected units to any of those groups
+Select all onscreen MML/Sniper or Direct Fire non exps or mobile AA or mobile AA, or Mobileshields/decievers
+Hotkeys to add selected units to any of those groups
 REQUIRES REUI
 ]]
 icon = "/mods/ImprovedSelection/icon.png"
